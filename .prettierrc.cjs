b91166d789ba2fc9fc4a74cfd53758cfba0f4614
jason lim <50891910+Sxxov@users.noreply.github.com>
2023-06-15 01:38:49 +0800

    Dawn of all commits

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+	trailingComma: 'all',
+	useTabs: true,
+	tabWidth: 4,
+	singleQuote: true,
+	jsxSingleQuote: true,
+	endOfLine: 'crlf',
+	singleAttributePerLine: false,
+	plugins: ['@prettier/plugin-php'],
+};
