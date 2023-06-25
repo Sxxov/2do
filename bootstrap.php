201dc5a01bbc1617492952a7c2e0ef0fcd44f63a
jason lim <50891910+Sxxov@users.noreply.github.com>
2023-06-26 05:05:30 +0800

    Add a basic catch-all for errors
    * Doesn't catch require or parse errors though

--- bootstrap.php
+++ bootstrap.php
@@ -1,22 +1,42 @@
 <?php
 
+use api\v1\lib\common\HttpCodes;
+use api\v1\lib\common\ResErr;
+use api\v1\lib\common\ResErrCodes;
+
 spl_autoload_register(function ($class) {
 	$baseDir = __DIR__;
 	$classPath = str_replace('\\', '/', $class);
 	$filePath = $baseDir . '/' . $classPath . '.php';
 
-	if (file_exists($filePath))
+	if (file_exists($filePath)) {
 		require_once $filePath;
+	}
 });
 
-$request_uri = preg_replace('/^\//', "", preg_replace('/\/$/', '', $_SERVER['REQUEST_URI']));
+$request_uri = preg_replace(
+	'/^\//',
+	'',
+	preg_replace('/\/$/', '', $_SERVER['REQUEST_URI']),
+);
 
-if ($request_uri === '/')
-	require 'index.php';
-else if (is_file($request_uri))
-	require $request_uri;
-else if (is_file($request_uri . '.php'))
-	require $request_uri . '.php';
-else if (is_file($request_uri . '/index.php'))
-	require $request_uri . '/index.php';
-else require '404.php';
+try {
+	if ($request_uri === '/') {
+		require_once 'index.php';
+	} elseif (is_file($request_uri)) {
+		require_once $request_uri;
+	} elseif (is_file($request_uri . '.php')) {
+		require_once $request_uri . '.php';
+	} elseif (is_file($request_uri . '/index.php')) {
+		require_once $request_uri . '/index.php';
+	} else {
+		require_once '404.php';
+	}
+} catch (Exception $err) {
+	return ResErr::send(
+		ResErrCodes::UNKNOWN,
+		http: HttpCodes::INTERNAL_SERVER_ERROR,
+		message: 'An unknown error occurred',
+		detail: $err,
+	);
+}
