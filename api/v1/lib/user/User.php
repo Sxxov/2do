1e01cb0f3dbd2a88083cb0f28ea28bd75e642fba
jason lim <50891910+Sxxov@users.noreply.github.com>
2023-06-26 05:07:04 +0800

    Add convenience functions into `User`

--- api/v1/lib/user/User.php
+++ api/v1/lib/user/User.php
@@ -2,20 +2,38 @@
 
 namespace api\v1\lib\user;
 
-class User
-{
+class User {
+	const ID_REGEX = '/^[a-zA-Z0-9-._]+$/';
+
 	public string $id;
 	public string $username;
 	public string $email;
 	public string $peepeepoopoo;
 
-	public static function from(array $data): User
-	{
-		$user = new User();
-		foreach ($data as $key => $value) {
-			$user->{$key} = $value;
-		}
+	public function __construct(
+		string $id,
+		string $username,
+		string $email,
+		string $peepeepoopoo = 'peepeepoopoo',
+	) {
+		$this->id = $id;
+		$this->username = $username;
+		$this->email = $email;
+		$this->peepeepoopoo = $peepeepoopoo;
+	}
 
-		return $user;
+	public static function isValidUsername(string $username): bool {
+		return strlen($username) >= 3 &&
+			strlen($username) <= 20 &&
+			preg_match(self::ID_REGEX, $username);
+	}
+
+	public static function isValidEmail(string $email): bool {
+		return strlen($email) <= 255 &&
+			filter_var($email, FILTER_VALIDATE_EMAIL);
+	}
+
+	public static function isValidPassword(string $password): bool {
+		return strlen($password) >= 8 && strlen($password) <= 255;
 	}
 }
