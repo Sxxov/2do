dd6279ecf5fa9d4079d1117431e0112b51a5dcfc
jason lim <50891910+Sxxov@users.noreply.github.com>
2023-06-26 05:05:57 +0800

    Add `ResOk#redirect`

--- api/v1/lib/common/ResOk.php
+++ api/v1/lib/common/ResOk.php
@@ -4,14 +4,19 @@ namespace api\v1\lib\common;
 
 class ResOk {
 	public array $data;
+	public ?string $redirect;
 
-	public function __construct($data) {
+	public function __construct($data, ?string $redirect = null) {
 		$this->data = (array) $data;
+
+		if ($redirect) {
+			$this->redirect = $redirect;
+		}
 	}
 
-	public static function send($data) {
+	public static function send($data, ?string $redirect = null) {
 		http_response_code(200);
-		$ok = new ResOk($data);
+		$ok = new ResOk($data, $redirect);
 		echo $ok;
 	}
 
