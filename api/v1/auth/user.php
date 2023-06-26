62cab61eaa455d255a220bcca4b29774ecc28057
jason lim <50891910+Sxxov@users.noreply.github.com>
2023-06-27 05:18:13 +0800

    Simplify `/api/v1/user`

--- api/v1/auth/user.php
+++ api/v1/auth/user.php
@@ -3,14 +3,5 @@
 namespace api\v1\auth;
 
 use api\v1\lib\auth\Authenticator;
-use api\v1\lib\common\ResErr;
-use api\v1\lib\common\ResErrCodes;
 
-session_start();
-$sessionId = session_id();
-
-if (!$sessionId) {
-	return (new ResErr(ResErrCodes::UNAUTHORISED))->echo();
-}
-
-return (new Authenticator())->getSessionUser($sessionId)->echo();
+return (new Authenticator())->getSessionUser()->echo();
