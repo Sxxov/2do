7dd8319d5d700bacce3d606ac5730252c2b581c6
jason lim <50891910+Sxxov@users.noreply.github.com>
2023-06-28 03:02:01 +0800

    Implement the rest of /app

--- app/index.js
+++ app/index.js
@@ -1,14 +1,20 @@
-import { X, css, html, until } from '../lib/common/x/X.js';
+import { X, css, html, until, spread, repeat } from '../lib/common/x/X.js';
 import '../lib/layout/Main.js';
+import { Button } from '../lib/components/Button.js';
 import './lib/components/AppCtaFragment.js';
 import './lib/components/AppMenuFragment.js';
 import '../lib/components/LoaderCircle.js';
 import '../lib/components/LoaderSkeleton.js';
 import './lib/components/NoteItem.js';
+import { Dropdown } from '../lib/components/Dropdown.js';
+import '../lib/components/Input.js';
 import { objT } from '../lib/common/lit/runtime/types/objT.js';
 import { stateT } from '../lib/common/lit/runtime/traits/stateT.js';
 import { Toaster } from '../lib/components/Toaster.js';
 import { Toast } from '../lib/components/Toast.js';
+import { numT } from '../lib/common/lit/runtime/types/numT.js';
+import { strT } from '../lib/common/lit/runtime/types/strT.js';
+import { Input } from '../lib/components/Input.js';
 
 /**
  * @typedef {{
@@ -45,19 +51,49 @@ export class AppRoute extends X {
 	 */
 	static properties = {
 		notesPromise: objT(stateT),
+		noteSortOrder: numT(stateT),
+		noteSortKind: strT(stateT),
+		noteSearchQuery: strT(stateT),
+	};
+
+	/** @typedef {(typeof AppRoute)['NoteSortKinds'][keyof (typeof AppRoute)['NoteSortKinds']]} NoteSortKinds */
+
+	static NoteSortKinds = /** @type {const} */ ({
+		DATE_CREATED: 'date-created',
+		DATE_MODIFIED: 'date-modified',
+		ALPHANUMERIC: 'alphanumeric',
+	});
+
+	static NoteSorters = {
+		[this.NoteSortKinds.DATE_CREATED]: (
+			/** @type {Note} */ a,
+			/** @type {Note} */ b,
+		) => (a.dateCreated < b.dateCreated ? -1 : 1),
+		[this.NoteSortKinds.DATE_MODIFIED]: (
+			/** @type {Note} */ a,
+			/** @type {Note} */ b,
+		) => (a.dateModified < b.dateModified ? -1 : 1),
+		[this.NoteSortKinds.ALPHANUMERIC]: (
+			/** @type {Note} */ a,
+			/** @type {Note} */ b,
+		) => a.title.localeCompare(b.title),
 	};
 
 	constructor() {
 		super();
 
 		/** @type {Promise<Note[]>} */ this.notesPromise;
+		/** @type {1 | -1} */ this.noteSortOrder = 1;
+		/** @type {NoteSortKinds} */ this.noteSortKind =
+			AppRoute.NoteSortKinds.DATE_MODIFIED;
+		/** @type {string} */ this.noteSearchQuery = '';
 	}
 
 	/** @override */
 	connectedCallback() {
 		super.connectedCallback();
 
-		this.#updateNotes();
+		this.notesPromise = this.#getNotes();
 	}
 
 	/** @override */
@@ -81,12 +117,6 @@ export class AppRoute extends X {
 		return user.data;
 	}
 
-	async #updateNotes() {
-		this.notesPromise = this.#getNotes();
-
-		await this.notesPromise;
-	}
-
 	/** @returns {Promise<Note[]>} */
 	async #getNotes() {
 		const user = await this.#getSessionUser();
@@ -105,6 +135,36 @@ export class AppRoute extends X {
 					.slice(0, 19)
 					.replace('T', ' '),
 				dateModified: new Date()
+					.toISOString()
+					.slice(0, 19)
+					.replace('T', ' '),
+				peepeepoopoo: 'peepeepoopoo',
+			},
+			{
+				id: '2',
+				title: 'ANote 2',
+				description: 'This is a noteeeeeee.',
+				owner: user.id,
+				dateCreated: new Date(Date.now() - 10000)
+					.toISOString()
+					.slice(0, 19)
+					.replace('T', ' '),
+				dateModified: new Date(Date.now() + 10000)
+					.toISOString()
+					.slice(0, 19)
+					.replace('T', ' '),
+				peepeepoopoo: 'peepeepoopoo',
+			},
+			{
+				id: '3',
+				title: 'ZNote 3',
+				description: 'This is a noteeeeeeeeeesdfsdfgsfee.',
+				owner: user.id,
+				dateCreated: new Date(Date.now() - 20000)
+					.toISOString()
+					.slice(0, 19)
+					.replace('T', ' '),
+				dateModified: new Date(Date.now() + 20000)
 					.toISOString()
 					.slice(0, 19)
 					.replace('T', ' '),
@@ -133,6 +193,19 @@ export class AppRoute extends X {
 		});
 	}
 
+	#createNote() {}
+
+	async #refreshNotes() {
+		const { cancel } = Toaster.toast('Refreshing notes…');
+
+		this.notesPromise = this.#getNotes();
+		await this.notesPromise;
+
+		cancel();
+
+		Toaster.toast('Notes refreshed', Toast.variants.ok);
+	}
+
 	/** @override */
 	render() {
 		return html`
@@ -140,14 +213,8 @@ export class AppRoute extends X {
 				<x-app-menu-fragment slot="menu"></x-app-menu-fragment>
 				<x-app-cta-fragment
 					slot="cta"
-					@refresh=${async () => {
-						const { cancel } = Toaster.toast('Refreshing notes...');
-
-						await this.#updateNotes();
-
-						cancel();
-
-						Toaster.toast('Notes refreshed!', Toast.variants.ok);
+					@refresh=${() => {
+						void this.#refreshNotes();
 					}}
 				></x-app-cta-fragment>
 				<div class="content">
@@ -173,22 +240,130 @@ export class AppRoute extends X {
 									</p>`,
 							),
 							html`<x-loader-circle
-								height="28px"
-								width="28px"
+								height="21px"
+								width="21px"
 							></x-loader-circle>`,
 						)}
 					</div>
+
 					<div class="notes">
+						<div class="toolbar">
+							<div class="search">
+								<x-input
+									placeholder="Search"
+									@input=${(
+										/**
+										 * @type {InputEvent & {
+										 * 	currentTarget: HTMLInputElement;
+										 * }}
+										 */ e,
+									) => {
+										this.noteSearchQuery =
+											e.currentTarget.value;
+									}}
+								></x-input>
+								<div class="sort">
+									<x-dropdown
+										width="100%"
+										.expanderVariant=${{
+											...Button.variants.flatRight,
+											...Button.variants.shadowSm,
+										}}
+										.items=${[
+											{
+												icon: 'today',
+												title: 'Modified',
+												value: AppRoute.NoteSortKinds
+													.DATE_MODIFIED,
+												selected: true,
+											},
+											{
+												icon: 'today',
+												title: 'Created',
+												value: AppRoute.NoteSortKinds
+													.DATE_CREATED,
+											},
+											{
+												icon: 'sort_by_alpha',
+												title: 'Alphabetical',
+												value: AppRoute.NoteSortKinds
+													.ALPHANUMERIC,
+											},
+										]}
+										@change=${(
+											/** @type {CustomEvent} */ e,
+										) => {
+											this.noteSortKind = e.detail.value;
+										}}
+									>
+									</x-dropdown>
+									<x-button
+										${spread(Button.variants.secondary)}
+										${spread(Button.variants.flatLeft)}
+										${spread(Button.variants.shadowSm)}
+										@click=${() => {
+											this.noteSortOrder *= -1;
+										}}
+										><x-i
+											>${this.noteSortOrder > 0
+												? 'arrow_downward'
+												: 'arrow_upward'}</x-i
+										></x-button
+									>
+								</div>
+							</div>
+						</div>
+
+						<div class="new">
+							<x-button
+								width="100%"
+								${spread(Button.variants.primary)}
+								${spread(Button.variants.shadowSm)}
+								@click=${() => {
+									this.#createNote();
+								}}
+								><x-i slot="left">sticky_note_2</x-i>Add a
+								note<x-i slot="right">add</x-i></x-button
+							>
+						</div>
 						${until(
 							this.notesPromise.then((notes) =>
-								notes.map(
-									(note) => html`
-										<x-note-item
-											title=${note.title}
-											description=${note.description}
-										></x-note-item>
-									`,
-								),
+								notes.length > 0
+									? repeat(
+											notes
+												.filter(
+													(v) =>
+														this.noteSearchQuery ===
+															'' ||
+														v.title
+															.toLowerCase()
+															.includes(
+																this.noteSearchQuery.toLowerCase(),
+															) ||
+														v.description
+															.toLowerCase()
+															.includes(
+																this.noteSearchQuery.toLowerCase(),
+															),
+												)
+												.sort(
+													(a, b) =>
+														AppRoute.NoteSorters[
+															this.noteSortKind
+														](a, b) *
+														this.noteSortOrder,
+												),
+											(note) => note.id,
+											(note) => html`
+												<x-note-item
+													title=${note.title}
+													description=${note.description}
+												></x-note-item>
+											`,
+									  )
+									: html`<p class="empty">
+											👌<br /><br />All done!
+									  </p>`,
 							),
 							html`<x-loader-skeleton
 									height="84px"
@@ -206,6 +381,16 @@ export class AppRoute extends X {
 									delay="300ms"
 								></x-loader-skeleton>`,
 						)}
+						<div class="refresh">
+							<x-button
+								${spread(Button.variants.secondary)}
+								@click=${() => {
+									void this.#refreshNotes();
+								}}
+							>
+								<x-i>refresh</x-i>
+							</x-button>
+						</div>
 					</div>
 				</div>
 			</x-main>
@@ -239,6 +424,64 @@ export class AppRoute extends X {
 				flex-direction: column;
 				gap: 14px;
 				max-width: 600px;
+				width: 100%;
+			}
+
+			.content > .notes > .toolbar {
+				position: sticky;
+				top: 98px;
+				height: 84px;
+
+				display: flex;
+				gap: 14px;
+
+				z-index: 1;
+			}
+
+			.content > .notes > .toolbar > .search {
+				display: flex;
+				gap: 14px;
+
+				margin: 0 -14px;
+				width: calc(100% + 28px);
+
+				background: var(----colour-background-primary);
+				box-shadow: var(----shadow-inner-sm), var(----shadow-sm);
+				border-radius: 56px;
+				padding: 14px;
+				box-sizing: border-box;
+			}
+
+			.content > .notes > .toolbar > .search > .sort {
+				display: flex;
+			}
+
+			.content > .notes > .toolbar > .actions {
+				display: flex;
+				gap: 14px;
+				align-items: center;
+			}
+
+			.content > .notes > .empty {
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				text-align: center;
+				color: var(----colour-text-secondary);
+				background: var(----colour-background-primary);
+				box-shadow: var(----shadow-inner-sm), var(----shadow-sm);
+				border-radius: 28px;
+				padding: 14px;
+				box-sizing: border-box;
+				margin: 0;
+				height: 336px;
+			}
+
+			.content > .notes > .refresh {
+				display: flex;
+				flex-direction: column;
+				align-items: center;
+
 				width: 100%;
 			}
 		`,
