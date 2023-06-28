124ccd8e479b55ae882f2a44286146d77d4e9107
matth10s <118879073+matth10s@users.noreply.github.com>
2023-06-28 16:22:19 +0800

    Updated Reminder.php

--- api/v1/lib/reminder/Reminder.php
+++ api/v1/lib/reminder/Reminder.php
@@ -2,31 +2,22 @@
 
     namespace api\v1\lib\reminder;
 
-use api\v1\lib\auth\Authenticator;
-use api\v1\lib\common\ResErr;
-use api\v1\lib\common\ResErrCodes;
-use api\v1\lib\common\ResOk;
-use api\v1\lib\db\Db;
-use api\v1\lib\db\DbInfo;
-use api\v1\lib\note\Note;
-use DateTime;
-use mysqli_sql_exception;
-
     class Reminder {
         public string $id;
         public string $re_title;
         public string $re_datetime;
+        public string $re_done;
 
         public function __construct(
             string $id,
             string $re_title,
             string $re_datetime,
-            string $re_isCompleted,
+            string $re_done,
         ) {
             $this->id = $id;
             $this->re_title = $re_title;
             $this->re_datetime = $re_datetime;
-            $this->re_isCompleted = $re_isCompleted;
+            $this->re_isCompleted = $re_done;
         }
     }
 
